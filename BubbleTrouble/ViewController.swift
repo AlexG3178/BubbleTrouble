f3df7fbaca67517a1b09286f63ce412e764e2c2e
alexg3178 <alexg3178@gmail.com>
2019-03-23 17:12:05 +0200

    BubbleTrouble

--- BubbleTrouble/ViewController.swift
+++ BubbleTrouble/ViewController.swift
@@ -8,15 +8,14 @@
 
 import Cocoa
 import SpriteKit
-import GameplayKit
 
 class ViewController: NSViewController {
-
+    
     @IBOutlet var skView: SKView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         if let view = self.skView {
             // Load the SKScene from 'GameScene.sks'
             if let scene = SKScene(fileNamed: "GameScene") {
